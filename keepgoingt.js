91933b41a7b8bb7e227f4e5e01cb860f14320ff9
Anshpreet <festiveworld7@gmail.com>
2024-01-09 21:37:58 +0530

    Create keepgoingt.js

---
+++ keepgoingt.js
@@ -0,0 +1,36 @@
+var target_date = new Date("January 10, 2024 00:00:00").getTime(); // set the countdown date to January 10, 2024
+var days, hours, minutes, seconds; // variables for time units
+
+var countdown = document.getElementById("tiles"); // get tag element
+
+getCountdown();
+
+setInterval(function () { getCountdown(); }, 1000);
+
+function getCountdown(){
+
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+
+    if (seconds_left < 0) {
+        countdown.innerHTML = "<span>00</span><span>00</span><span>00</span><span>00</span>"; // countdown reached
+        return;
+    }
+
+    days = pad( parseInt(seconds_left / 86400) );
+    seconds_left = seconds_left % 86400;
+
+    hours = pad( parseInt(seconds_left / 3600) );
+    seconds_left = seconds_left % 3600;
+
+    minutes = pad( parseInt(seconds_left / 60) );
+    seconds = pad( parseInt( seconds_left % 60 ) );
+
+    // format countdown string + set tag value
+    countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>"; 
+}
+
+function pad(n) {
+    return (n < 10 ? '0' : '') + n;
+}
